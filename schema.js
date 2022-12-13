d65e0a37e813d0fef7ba13ffe7a6df41c8d4b953
ZugustheDugus <h1cooper2019@protonmail.com>
2022-12-12 19:44:51 -0600

    schemas for mongodb implementation

--- schema.js
+++ schema.js
@@ -3,12 +3,15 @@ const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
   name: String,
   location: String,
+  email: String,
   id: Number,
+  createdOn: String,
 })
 
 const parkSchema = new mongoose.Schema({
   name: String,
   location: String,
+  jurisdiction: String,
   id: Number,
 })
 
